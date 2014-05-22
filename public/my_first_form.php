37a393662fa74c75b34e3dfad0a8cd9b8a751718
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-22 15:58:10 -0500

    Added placeholder and <button>

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -11,19 +11,19 @@
     			?>
     			<form method="POST" action="/my_first_form.php">
     				<p>
-        			<label for="username">Username</label>
-        			<input id="username" name="username" type="text">
+        				<label for="username">Username:</label>
+        				<input id="username" name="username" type="text"placeholder="Enter your username"autofocus>
     				</p>
     				<p>
-        			<label for="password">Password</label>
-        			<input id="password" name="password" type="password">
+        				<label for="password">Password:</label>
+        				<input id="password" name="password" type="password"placeholder="Enter your password">
     				</p>
     				<p>
-    				<label for=“remember”>Remember Me?</label>
-					<input id =“remember name="memory" type="checkbox">
+    					<label for=“remember”>Remember Me?</label>
+						<input id =“remember name="memory" type="checkbox">
     				</p>
     				<p>
-        			<input type="submit">
+        				<button type="submit"> Log In</button>
     				</p>
 				</form>
 			</body>
