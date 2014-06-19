0f36e0837d4c99491d3654cbe029dd3623dbf3ec
malloryweatherston <mjw0909@sbcglobal.net>
2014-06-19 11:20:31 -0500

    added strtolower() for files

--- public/classes/address_data_store.php
+++ public/classes/address_data_store.php
@@ -1,39 +1,28 @@
 <?
-class AddressDataStore {
+require_once('classes/filestore.php');
 
-    public $filename = '';
+class AddressDataStore extends Filestore{
+
 
     public function __construct($filename = 'address_book.csv')
-    {
-    	$this->filename = $filename;
+    {	
+    	$filename = strtolower($filename);
+    	parent::__construct($filename);
+    	echo $this->filename; 
+
     }
 
     public function read_address_book()
     {
-        // Code to read file $this->filename
-
-			$entries = [];
-			$handle = fopen($this->filename, 'r');
-			while(!feof($handle)) {
-				$row = fgetcsv($handle);
-				if(is_array($row)) {
-					$entries[] = $row;
-				}
-			}
-			fclose($handle);
-			return $entries;
+        
     }
 
     public function write_address_book($big_array) 
     {
-        // Code to write $addresses_array to file $this->filename
-        $handle = fopen($this->filename, 'w');
-		foreach($big_array as $fields) {
-			fputcsv($handle, $fields); 
-		}
-		fclose($handle);
 
-    }
-
+	}
 
 }
+
+
+
